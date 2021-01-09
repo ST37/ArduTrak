fcadf2fb0f074c4eb4c386022ecf98d47fbb9ed1
Michael Franke <Michaelfrankeathome@gmail.com>
2021-01-09 02:02:21 +0100

    Update ArduTrak.ino
    New version.
    Internally, it is now calculated with milliseconds and passed on to the servo bibliotek.
    Deadzone is adapted accordingly
    Two drive modes are ready.
    Debugging is set to the graphical output for the plotter in Arduino.
    The direction of rotation of the motors can now be set using variables.

--- ArduTrak.ino
+++ ArduTrak.ino
@@ -1,96 +1,129 @@
 #include "SBUS.h"
 #include <Servo.h>
-#include <EasyNeoPixels.h>
-
-// a SBUS object, which is on hardware
-// serial port 1#include <EasyNeoPixels.h>
 SBUS XM(Serial1);
-
 Servo Rservo;
 Servo Lservo;
-int Tval;
-int Sval;
-int DMode;
-int Rweel,TrimmR,Rraw;
-int Lweel,TrimmL,Lraw;
+
+int Tval; //Thrust variable 
+int Sval; //Steering variable
+int S1,S2; // Switches for Mode and Light
+int DMode; // Drive Mode variable
+int Rweel,Rraw;
+int Lweel,Lraw;
+int TrimmL,TrimmR;
 int DZ;
-bool LEDS;
+int MDR,MDL;
+
+const int Dpwm = 1000;
+const int Mpwm = 1500;
+const int Upwm = 2000; 
+
+bool CalibS;
 
 // channel, fail safe, and lost frames data
 uint16_t channels[16];
 bool failSafe;
 bool lostFrame;
+bool Debug;
+int t=0;
 
-const int ledPin =  9;
 
 void setup() {
-  // begin the SBUS communication
+  // Begin the SBUS communication
   XM.begin();
-  Serial.begin(115200);
-  Rservo.attach(5);
-  Lservo.attach(6);
-  setupEasyNeoPixels(15, 1);
-
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
+  // Serial for Debugging
+  Serial.begin(9600);
+  
+  //Drive Mode
   DMode = 1;
-  TrimmR = -11;
-  TrimmL = -1;
-  Rweel = 90 + TrimmR;
-  Lweel = 90 + TrimmL;
-  DZ = 2 ;
-  LEDS = false;
+  
+  // Motor configuration right
+  Rservo.attach(5); // Hardware pin for the right motor
+  TrimmR = 0;       // Trimm
+  MDR = -1;         // direction of the right motor
+  Rweel = Mpwm + TrimmR; //Start setting
+  
+  // Motor configuration left
+  Lservo.attach(6); // Hardware pin for left motor
+  TrimmL = 0;       // Trimm
+  MDL = -1;         // direction of the left motor
+  Lweel = Mpwm + TrimmL; //Start setting
+  
+  // Deadzone 
+  DZ = 10 ;
+  //Debugging Mode thrue -> Channel's to Serial Port
+  Debug = true; 
+  // Calibration Mode
+  CalibS = false;
 }
 
 void loop() {
-
-  // look for a good SBUS packet from the receiver
-  if(XM.read(&channels[0], &failSafe, &lostFrame)){
-    if(channels[4] < 1000) DMode = 1;
-    if(channels[4] > 1000) DMode = 2;
-    Tval = map(channels[2], 172, 1811, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-    Sval = map(channels[1], 172, 1811, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  // SBUS signal processing
+  if(!CalibS & (XM.read(&channels[0], &failSafe, &lostFrame))){
+    Tval = map(channels[2], 172, 1811, Dpwm, Upwm);    
+    Sval = map(channels[1], 172, 1811, Dpwm, Upwm);    
+    S1 = map(channels[4], 172, 1811, Dpwm, Upwm); 
+    S2 = map(channels[5], 172, 1811, Dpwm, Upwm);
     
-    if(DMode == 1){
-      Rraw = ((180-Tval)-(90-Sval));
-      Lraw = (Tval-(90-Sval));
+    // Drive Mode 1 Normal
+    if(S1 < 1500){
+      Rraw = Mpwm-(((Tval-(Sval-Mpwm)-Mpwm)));
+      Lraw = Mpwm+(((Tval+(Sval-Mpwm)-Mpwm)));
       }
       
-    if(DMode == 2){
-      if (Sval >= (90+DZ)) Rraw =90 + (90-Sval);
-      if (Sval <= (90-DZ)) Lraw =90 - (Sval-90) ;
-      if (Sval <(90+DZ) & Sval > (90-DZ)){
-        Rraw = 90;
-        Lraw = 90;
+    // Drive Mode 2 Crawling 
+    if(S1 > 1500){
+      if (Sval >= (Mpwm+DZ)){ 
+        Lraw = Mpwm - (Mpwm-Tval);
+        Rraw = Mpwm;
+      }
+      if (Sval <= (Mpwm-DZ)){ 
+        Rraw = Mpwm - (Tval-Mpwm) ;
+        Lraw = Mpwm;
+      }
+      
+      if ((Sval <(Mpwm+DZ)) & (Sval > (Mpwm-DZ))){
+        Rraw = Mpwm;
+        Lraw = Mpwm;
+        } 
       }
 
-      
-     
-      }
 
-      Rweel = Rraw + TrimmR;
-      Lweel = Lraw + TrimmL;
-      
-    Rservo.write(Rweel);
-    Lservo.write(Lweel);
+    // Add Deadzone 
+    if((Rraw < Mpwm+ DZ) & (Rraw > Mpwm- DZ)) Rraw = Mpwm;
+    if((Lraw < Mpwm+ DZ) & (Lraw > Mpwm- DZ)) Lraw = Mpwm; 
+    // Add Trimm and and Motor direction
+    Rweel = Mpwm + ((Mpwm - (Rraw + TrimmR))*MDR);
+    Lweel = Mpwm + ((Mpwm - (Lraw + TrimmL))*MDL);      
+    Rservo.writeMicroseconds(Rweel);
+    Lservo.writeMicroseconds(Lweel);
+
+
+
     
     }
-    Serial.print("Mode = ");
-    Serial.print(DMode);  
-    
-    Serial.print(" Tval = ");
+  // Debuggingoutput via serial port
+  if(Debug & (t>1000)){    
+    Serial.print("Min:1000, ");
     Serial.print(Tval);  
-    Serial.print("  Sval = ");
+    Serial.print(",");
+    //Serial.print("Sval:");
     Serial.print(Sval);
-
-    Serial.print(" Rweel = ");
+    Serial.print(",");
+    //Serial.print("Rweel:");
     Serial.print(Rweel);  
-    Serial.print("  Lweel = ");
-    Serial.println(Lweel);
-   if(LEDS)  writeEasyNeoPixel(0, HIGH);
-   
-  
+    Serial.print(",");
+    //Serial.print("Lweel:");
+    Serial.print(Lweel);
+    Serial.print(",");
+    Serial.println("Max:2000");
+    if(t>1000) t = 0;
+    
+  }
+  t++;
 }
+
+
 int expo(float pos, float amount)
 {
  pos = pos / 100;
